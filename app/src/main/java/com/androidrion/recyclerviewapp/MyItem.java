0b221a8d5f3b304d042bca33f63ae876935a6c81
Masfauzi22 <chiekiepoygaming@gmail.com>
2022-03-20 16:21:16 +0800

    tugas recycleview pesawat

---
+++ app/src/main/java/com/androidrion/recyclerviewapp/MyItem.java
@@ -0,0 +1,23 @@
+package com.androidrion.recyclerviewapp;
+
+class MyItem {
+
+    static int[] iconList = {
+            R.drawable.garuda,R.drawable.uia,
+            R.drawable.turkis, R.drawable.lion, R.drawable.spanair, R.drawable.singapore, R.drawable.qatar,
+            R.drawable.nam, R.drawable.malaysia, R.drawable.sriwijaya, R.drawable.batik, R.drawable.airasia,
+            R.drawable.latam, R.drawable.iberia, R.drawable.lufthansa, R.drawable.france,
+
+
+    };
+
+    static String[] Headline = {"garuda", "UIA", "Turkish airlines", "Lion Air", "Spanair", "Singapore Airlines",
+            "Qatar Airways", "NAM air", "Malaysia Airlines", "Sriwijaya Air", "Batik Air", "Air Asia", "Latam Airlines",
+            "Iberia airlines", "Lufthansa Airlines", "France Airlines"
+    };
+
+    static String[] Subhead = {"pesawat","pesawat","pesawat","pesawat","pesawat"
+            ,"pesawat","pesawat","pesawat","pesawat","pesawat","pesawat","pesawat","pesawat","pesawat","pesawat"
+            ,"pesawat"
+    };
+}
